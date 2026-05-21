c8b7769d2ca25f0f9d7f9b11449d016644b34c00
Miguel Mena <angelmenaepo90@gmail.com>
2026-05-21 11:33:01 -0600

    4: Crear servidor básico con express

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log('Hello, World!');
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => res.send('Hola Mundo'));
+
+app.listen(3000, () => console.log('Servidor corriendo en puerto 3000'));
