c337fec5476dea383601a25ba452fe84231c39a4
Miguel Mena <angelmenaepo90@gmail.com>
2026-05-21 11:51:38 -0600

    7: Agregar ruta /about

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => res.send('Hola Mundo'));
+app.get('/about', (req, res) => res.send('Acerca de'));
 
 app.listen(3000, () => console.log('Servidor corriendo en puerto 3000'));
